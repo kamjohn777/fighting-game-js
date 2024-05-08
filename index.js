a9f799d0882b5fa95be7ee2de5f0061cdc7fc2ab
Kameron Johnson <kamjohn79.kdj@gmail.com>
2024-05-08 13:36:38 -0500

    working on the attack collison

--- index.js
+++ index.js
@@ -13,16 +13,33 @@ $(document).ready(function () {
   const gravity = 0.7;
 
   class Sprite {
-    constructor({ position, velocity }) {
+    constructor({ position, velocity, color = "red" }) {
       this.position = position;
       this.velocity = velocity;
       this.height = 150;
+      this.width = 50;
       this.lastKey;
+      this.color = color;
+      this.attackBox = {
+        position: this.position,
+        width: 100,
+        height: 50,
+      };
+      this.isAttacking = false;
     }
 
     draw() {
-      c.fillStyle = "red";
-      c.fillRect(this.position.x, this.position.y, 50, this.height);
+      c.fillStyle = this.color;
+      c.fillRect(this.position.x, this.position.y, 50, this.height, this.width);
+
+      //   Attack Box
+      c.fillStyle = "purple";
+      c.fillRect(
+        this.attackBox.position.x,
+        this.attackBox.position.y,
+        this.attackBox.width,
+        this.attackBox.height
+      );
     }
 
     update() {
@@ -30,11 +47,22 @@ $(document).ready(function () {
       this.position.x += this.velocity.x;
       this.position.y += this.velocity.y;
 
+      // Update the position of the attack box
+      this.attackBox.position.x = this.position.x;
+      this.attackBox.position.y = this.position.y;
+
       if (this.position.y + this.height + this.velocity.y >= canvas[0].height) {
         this.velocity.y = 0;
       } else {
         this.velocity.y += gravity;
       }
+    }
+
+    attack() {
+      this.isAttacking = true;
+      setTimeout(() => {
+        this.isAttacking = false;
+      }, 100);
     }
   }
 
@@ -58,6 +86,7 @@ $(document).ready(function () {
       x: 0,
       y: 0,
     },
+    color: "green",
   });
 
   console.log(player);
@@ -80,7 +109,7 @@ $(document).ready(function () {
     },
   };
 
-//   let lastKey;
+  //   let lastKey;
 
   function animate() {
     window.requestAnimationFrame(animate);
@@ -104,6 +133,19 @@ $(document).ready(function () {
     } else if (keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight") {
       enemy.velocity.x = 5;
     }
+
+    // Detecting collision
+    if (
+      player.attackBox.position.x + enemy.attackBox.width >= enemy.position.x &&
+      player.attackBox.position.x <= enemy.position.x + enemy.width &&
+      player.attackBox.position.y + player.attackBox.height >=
+        enemy.position.y &&
+      player.attackBox.position.y <= enemy.position.y + enemy.height &&
+      player.isAttacking
+    ) {
+        player.isAttacking = false;
+      console.log("collision");
+    }
   }
 
   animate();
@@ -121,6 +163,9 @@ $(document).ready(function () {
         break;
       case "w":
         player.velocity.y = -20;
+        break;
+      case " ":
+        player.attack();
         break;
 
       // Enemy Keys
